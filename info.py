f048fe6e9ba094ffb1238dd34f1465311dd25379
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2024-12-04 15:31:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '2054877'))
 API_HASH = environ.get('API_HASH', '4227c1e45e462209a3dcc67ada88a44f')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6420054947:AAGx4KP_UwaUP_v1m-_p89cfPW-Smy6jOmA")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7499058839:AAHJ-zxAT8zT845ejdthVbg3nG5vavC1MKY")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
